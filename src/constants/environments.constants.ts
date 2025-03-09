198bc85d72de851c7e236b1c2dc63164cb3485db
“ALVINdimpos” <alvindimpos@gmail.com>
2025-03-10 00:13:55 +0200

    Update default API and base URLs for deployment

--- src/constants/environments.constants.ts
+++ src/constants/environments.constants.ts
@@ -1,6 +1,8 @@
 export const {
-  VITE_LOCAL_API_URL: localApiUrl,
+  VITE_LOCAL_API_URL:
+    localApiUrl = "https://url-shortener-backend-5qi6.onrender.com",
   VITE_STAGING_API_URL: stagingApiUrl,
   VITE_PRODUCTION_API_URL: productionApiUrl,
-  VITE_APP_BASE_URL: baseUrl,
+  VITE_APP_BASE_URL:
+    baseUrl = "https://url-shortener-frontend-w7px.onrender.com",
 } = import.meta.env;
